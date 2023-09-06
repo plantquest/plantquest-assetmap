727c4aac536f5d235a17fdf77df0ae7fd7ca9b17
Richard Rodger <richard@ricebridge.com>
2023-09-06 14:55:53 +0100

    v6.0.0

--- dist/pqam.umd.js
+++ dist/pqam.umd.js
@@ -13329,7 +13329,7 @@ var __async = (__this, __arguments, generator) => {
   var Leaflet_EditableExports = Leaflet_Editable$2.exports;
   const Leaflet_Editable$1 = /* @__PURE__ */ getDefaultExportFromCjs(Leaflet_EditableExports);
   const name = "@plantquest/assetmap";
-  const version = "5.0.1";
+  const version = "6.0.0";
   const description = "PlantQuest Asset Map";
   const author = "plantquest";
   const license = "MIT";
