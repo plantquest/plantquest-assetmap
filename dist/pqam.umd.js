8c85c43f8477583e95d3fbd14acd0196f1febc7e
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2024-04-11 15:34:20 +0200

    v9.0.0

--- dist/pqam.umd.js
+++ dist/pqam.umd.js
@@ -13143,7 +13143,7 @@ var __async = (__this, __arguments, generator) => {
   var Leaflet_EditableExports = Leaflet_Editable$2.exports;
   const Leaflet_Editable$1 = /* @__PURE__ */ getDefaultExportFromCjs(Leaflet_EditableExports);
   const name = "@plantquest/assetmap";
-  const version = "8.0.0";
+  const version = "9.0.0";
   const description = "PlantQuest Asset Map";
   const author = "plantquest";
   const license = "MIT";
